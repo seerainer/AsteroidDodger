b6f46cbf71652d424036aa9bec2ec178778d63a4
Philipp Seerainer <philipp@seerainer.com>
2025-11-19 21:26:34 +0100

    Update Gradle and dependencies; add gradle-daemon-jvm.properties

--- src/main/java/io/github/seerainer/game/Main.java
+++ src/main/java/io/github/seerainer/game/Main.java
@@ -1,6 +1,10 @@
 package io.github.seerainer.game;
 
 public class Main {
+    private Main() {
+	throw new IllegalStateException("Main class");
+    }
+
     public static void main() {
 	new GameApp().start();
     }
